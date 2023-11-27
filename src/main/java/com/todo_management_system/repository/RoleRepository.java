159c23b70c0a2f6f84fc5bb0151d533dc8707943
Raju Gharti <gharti.rg@gmail.com>
2023-11-27 14:03:34 -0500

    implement database based authentication and authorization

---
+++ src/main/java/com/todo_management_system/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.todo_management_system.repository;
+
+import com.todo_management_system.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
